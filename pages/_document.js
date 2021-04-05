e4aba26a5af888674bcf0b49a560eb7c228d1b62
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-05 20:52:57 +0100

    api key changed

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
 
           <script
             src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
-            data-api-key="YmJiYjA1ZDUtMWE4Ny00YzMyLWJlOGUtZDYxYmU2YjhkNGIzNjM3NTA0MDMzNTIyNTEzOTA1"
+            data-api-key="OTNlMWUwMDEtMzJhNS00ODdkLWFjZDItNjJjODI2NzRhNzQxNjM3NTA0MDMzNTIyNTEzOTA1"
             id="snipcart"
           ></script>
           <NextScript />
