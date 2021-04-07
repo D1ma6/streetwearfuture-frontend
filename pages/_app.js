1bd454a2be822cfde1bcfa91a7d5d54fea413697
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-07 19:10:30 +0100

    used library to add facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,23 @@ import Layout from "../components/Layout";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
+import Head from "next/head";
+
+function FacebookPixel() {
+  React.useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init("761362134767053");
+        ReactPixel.pageView();
+
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+  return null;
+}
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -18,6 +35,7 @@ function MyApp({ Component, pageProps }) {
   }, [router.events]);
   return (
     <Layout>
+      <FacebookPixel />
       <Component {...pageProps} />
     </Layout>
   );
