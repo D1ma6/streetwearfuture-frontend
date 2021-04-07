3e009f906bc67bd3570447fe8690d4db1b8d67aa
Dmytro Bula <d1ma6@Dmytros-MacBook-Pro.local>
2021-04-07 19:02:12 +0100

    changed the pos of the facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -35,46 +35,6 @@ class MyDocument extends Document {
           `,
             }}
           />
-        </Head>
-
-        <body>
-          <Main />
-
-          {/* <script>
-      !(function (f, b, e, v, n, t, s) {
-        if (f.fbq) return;
-        n = f.fbq = function () {
-          n.callMethod
-            ? n.callMethod.apply(n, arguments)
-            : n.queue.push(arguments);
-        };
-        if (!f._fbq) f._fbq = n;
-        n.push = n;
-        n.loaded = !0;
-        n.version = "2.0";
-        n.queue = [];
-        t = b.createElement(e);
-        t.async = !0;
-        t.src = v;
-        s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s);
-      })(
-        window,
-        document,
-        "script",
-        "https://connect.facebook.net/en_US/fbevents.js"
-      );
-      fbq("init", "761362134767053");
-      fbq("track", "PageView");
-    </script>
-    <noscript
-      ><img
-        height="1"
-        width="1"
-        style="display: none"
-        src="https://www.facebook.com/tr?id=761362134767053&ev=PageView&noscript=1"
-    /></noscript> */}
-
           <script
             dangerouslySetInnerHTML={{
               __html: `!(function (f, b, e, v, n, t, s) {
@@ -114,6 +74,45 @@ class MyDocument extends Document {
           />`,
             }}
           />
+        </Head>
+
+        <body>
+          <Main />
+
+          {/* <script>
+      !(function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = "2.0";
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        "script",
+        "https://connect.facebook.net/en_US/fbevents.js"
+      );
+      fbq("init", "761362134767053");
+      fbq("track", "PageView");
+    </script>
+    <noscript
+      ><img
+        height="1"
+        width="1"
+        style="display: none"
+        src="https://www.facebook.com/tr?id=761362134767053&ev=PageView&noscript=1"
+    /></noscript> */}
 
           <script
             src="https://code.jquery.com/jquery-3.4.1.min.js"
